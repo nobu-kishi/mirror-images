69543195a4a1d9c3da678badb213d1f67dd4b9b9
kishi <shore.tf@gmail.com>
2025-11-04 19:28:47 +0900

    refactor: スクリプトの変数記載方法を統一

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -5,5 +5,5 @@ aws ecr get-login-password --region "${AWS_REGION}" \
   | docker login --username AWS --password-stdin "${ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 
 docker pull "${ORIGINAL}"
-docker build -t "$TARGET" "$CONTEXT_DIR"
+docker build -t "${TARGET}" "${CONTEXT_DIR}"
 docker push "${TARGET}"
